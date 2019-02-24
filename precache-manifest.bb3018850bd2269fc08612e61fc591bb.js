47c9579c8e2e7325b4fe72b53ae289f680bb728e
Muslim Al Fatih <muslimalfatih22@gmail.com>
2019-02-25 04:16:59 +0700

    Updates

---
+++ precache-manifest.bb3018850bd2269fc08612e61fc591bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f456ee18b3a4816a80f0e12f6c6c5889",
+    "url": "/ghibli-studio/static/media/studio-ghibli-logo.f456ee18.png"
+  },
+  {
+    "revision": "5ef84402801def7c550e",
+    "url": "/ghibli-studio/static/js/runtime~main.5ef84402.js"
+  },
+  {
+    "revision": "87820894882f65b66420",
+    "url": "/ghibli-studio/static/js/main.87820894.chunk.js"
+  },
+  {
+    "revision": "7825fd719426cd698d02",
+    "url": "/ghibli-studio/static/js/2.7825fd71.chunk.js"
+  },
+  {
+    "revision": "87820894882f65b66420",
+    "url": "/ghibli-studio/static/css/main.94869000.chunk.css"
+  },
+  {
+    "revision": "7825fd719426cd698d02",
+    "url": "/ghibli-studio/static/css/2.e41d77c6.chunk.css"
+  },
+  {
+    "revision": "b18ced07d50f4355dc09757ee4963179",
+    "url": "/ghibli-studio/index.html"
+  }
+];
